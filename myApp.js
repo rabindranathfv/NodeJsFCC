c802ac3549efad9a67734be5040a9bc30137accb
Rabindranath Ferreira <ferreiralox@gmail.com>
2019-12-05 23:32:41 -0300

    full fcc node challenges

--- myApp.js
+++ myApp.js
@@ -2,46 +2,81 @@ var express = require('express');
 var app = express();
 
 // --> 7)  Mount the Logger middleware here
+const bodyParser = require('body-parser');
 
+// parse applicattion /x-www/form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 // --> 11)  Mount the body-parser middleware  here
 
 
 /** 1) Meet the node console. */
-console.log('Hello World');
+console.log("Hello World");
+
+app.use((req, res, next) => {
+    console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+    next();
+});
+
 
 /** 2) A first working Express Server */
-
+app.get('/', function(req, res) {
+    res.send('Hello World');
+})
 
 /** 3) Serve an HTML file */
-
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+})
 
 /** 4) Serve static assets  */
-
+app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */
-
+app.get('/json', function(req, res) {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({ "message": "HELLO JSON" });
+    }
+    res.json({ "message": "Hello json" });
+})
 
 /** 6) Use the .env file to configure the app */
-
+process.env.MESSAGE_STYLE = 'uppercase';
 
 /** 7) Root-level Middleware - A logger */
 //  place it before all the routes !
 
-
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({ time: req.time });
+});
 
 /** 9)  Get input from client - Route parameters */
-
+app.get('/:word/echo', (req, res) => {
+    res.json({ echo: req.params.word });
+})
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.route('/name').get((req, res) => {
+    let first = req.query.first;
+    let last = req.query.last;
+    let jsonObj = { name: `${first} ${last}` };
+    res.send(jsonObj);
+}).post((req, res) => {
+    let first = req.body.first;
+    let last = req.body.last;
+    let jsonObj = { name: `${first} ${last}` };
+    res.send(jsonObj);
+});
 
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
-
 
 /** 12) Get data form POST  */
 
